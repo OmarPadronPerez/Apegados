35dd8e6b0d64dca5dc51302c675187707175d3ef
OmarPadronPerez <omar.padron.perez@gmail.com>
2022-08-04 16:59:50 -0500

    add contacto
    contacto y barra pegadisa del index

---
+++ js/contacto.js
@@ -0,0 +1,12 @@
+function sendEmail() {
+    var nombre= document.getElementById('nombre').value;
+    var correo=document.getAnimations('correo').value;
+    var telefono=document.getElementById('telefono').value;
+    var mensaje=document.getElementById('mensaje').value;
+
+
+    console.log(nombre+"\n"+correo+"\n"+telefono+"\n"+mensaje);
+
+    
+    
+  }
---
+++ js/ins_regla.js
@@ -0,0 +1,14 @@
+var divRegla=document.getElementById('ins_regla');
+var telefono=0000000000;
+var contenido='<div class="row" id="regla">';
+//var content = document.createTextNode('<div class="row" id="regla"><div class="col-1">1</div></div>');
+
+   /* for(var i=0;i<12;i++){
+         contenido+='<div class="col-1">'+ (i+1) +'</div>';
+        //contenido+='<div class="col-1">' &i+1& '</div>';
+    }
+    contenido+='</div>';
+    console.log(contenido);
+
+    divRegla.insertAdjacentHTML('beforeend',contenido);*/
+
